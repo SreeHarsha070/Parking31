f3043e9e50fb31355cce1bb35facee53a487bcbd
Sri Harsha Konatham <harshakon8@gmail.com>
2022-07-19 14:40:44 +0530

    Update Cards.js

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -22,11 +22,12 @@ function Cards() {
 
     const handleClose = () => setShow(false);
     
-    const [range, setRange] = useState(50);
+    const [range, setRange] = useState(10);
     const [cardata, setCardata] = useState([])
-    const [allocatedList, setAllocatedList] = useState([2, 8, 10]);
+    const [allocatedList, setAllocatedList] = useState([]);
     const emprtyslots = [];
     const [selectslotnum, setSelectslotnum] = useState(0);
+    const [min, Setmin] = useState(1);
     const [name, setName] = useState("")
     const [mobile, setMobile] = useState("")
     const [slotlist, setSlotlist] = useState({
@@ -42,37 +43,35 @@ function Cards() {
 
 const nameslist = [];
 const arr = [];
-nameslist[2] = 'ravi';
-nameslist[8] = 'harsha';
-nameslist[10] = 'kiran';
-let min = 1;
 
 
-for(let i=1;i<=range;i++){
+
+
+
+const getPosts = async () => {
+  try {
+    for(let i=1;i<=range;i++){
     
-    if(allocatedList.includes(i) == true ){
-        arr[i] = {'id':i,'title':'Slot '+i,'name':nameslist[i],'status':1};  
-        //setSlotlist(slotlist => [...slotlist,  {'id':i,'title':'Slot '+i,'name':nameslist[i],'status':1})
-        //setSlotlist(slotlist => ({ ...slotlist, ["id"]: i,["title"]: 'Slot '+i, ["name"]: nameslist[i], ["status"]: 1 }));
+      if(allocatedList.includes(i) == true ){
+          arr[i] = {'id':i,'title':'Slot '+i,'name':nameslist[i],'status':1};  
+      }else if(allocatedList.includes(i) == false ){
+          
+          arr[i] = {'id':i,'title':'Slot '+i,'name':null,'status':0};
+      }
+      setDataList(arr);
       
-    }else if(allocatedList.includes(i) == false ){
-        
-        arr[i] = {'id':i,'title':'Slot '+i,'name':null,'status':0};
-        //setSlotlist(slotlist => [...slotlist,  {'id':i,'title':'Slot '+i,'name':null,'status':0})
-        //setSlotlist(slotlist => ({ ...slotlist, ["id"]: i,["title"]: 'Slot '+i, ["name"]: null, ["status"]: 0 }));
-       
-    }
-    
-}
+  }
+  
+  } catch (err) {
+    console.error(err.message);
+  }
+};
 
 useEffect(() => {
-  arr.map((slot, index)=>{
-       
+  getPosts()
 
-         setDataList(slot)
-     
-  })
 },[])
+//console.log(dataList);
 //setSlotlist(arr);
  const addMore = ()=>{
     setRange(range + 10)
@@ -80,15 +79,15 @@ useEffect(() => {
 
 
 const handleShow = (num) => {
-    //console.log(min);
-    //console.log(num);
+    console.log(min);
     if(min < num ){
       alert(`Slot ${min} is available.  Please choose numeric order only.`)
     }else{
       setShow(true);
       const index = emprtyslots.indexOf(num)
       setSelectslotnum(num);
-      min = num;
+      Setmin(num);
+      
     }
     
     
@@ -105,11 +104,17 @@ const movetoFree = (num) => {
   //console.log(index)
    
       allocatedList.splice(index, 1)
-      arr[num].status = 0;
-      setAllocatedList(allocatedList)
+      setAllocatedList(allocatedList => [...allocatedList, num])
       alert(`The  slot ${num} is now available to book`)
+      dataList[num].status = 0;
+      //console.log(dataList[num]);
+      popover(dataList[num])
+      setDataList(dataList);
+      if(min > num){
+        Setmin(num);
+      }
       
-    
+      
 
 }
 
@@ -118,7 +123,20 @@ const onSubmitHandler = (event) => {
     event.preventDefault();
     alert(`The Person : ${name} Successfully booked slot ${selectslotnum} `)
     setAllocatedList(allocatedList => [...allocatedList, selectslotnum])
-    arr[selectslotnum].name = name;
+    
+    dataList[selectslotnum].name = name;
+    dataList[selectslotnum].status = 1;
+    //setDataList(dataList);
+    setDataList(dataList)
+    // setDataList(dataList => {
+    //   // Object.assign would also work
+    //   return {...dataList, dataList};
+    // });
+    console.log(dataList[selectslotnum])
+    //setSelectslotnum(num);
+    Setmin(min + 1)
+    
+    
     setShow(false);
     setName("");
     setMobile("");
@@ -129,7 +147,7 @@ const onSubmitHandler = (event) => {
     <Popover id="popover-basic">
       <Popover.Header as="h3">Menu</Popover.Header>
       <Popover.Body>
-    {/* {data.title} */}
+   
     {data.status==1 ? 
     <p> {data.name} Already Booked <br /><br></br><span>Options : <Button variant="primary" onClick={()=>{movetoFree(data.id)}}>
     Move to free
@@ -148,11 +166,11 @@ return (
     <Row>
         <h3 style={{textAlign:"center",marginTop:"20px",marginBottom:"20px",textDecoration:"underline"}}>Parking Management System</h3>
         {
-            arr.map((slot, index)=>{
+            dataList.map((slot, index)=>{
                 return(
                   <Col style={{marginTop:"10px"}} md={2} key={index}>
                     <div className="ranga1">
-                        <div className="border1 top text-center"> {slot.title} 
+                        <div className="top text-center"> {slot.title} 
                         
                     <OverlayTrigger trigger="click" rootClose  placement="right" overlay={popover(slot)}>
                     <Button variant="success">Book Slot</Button>
